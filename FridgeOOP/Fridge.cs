63133ba51155ae7e5fb728f513559d96e0474747
TAxford <t.axford2@yahoo.com>
2018-01-31 15:21:32 -0500

    Added more methods

--- FridgeOOP/Fridge.cs
+++ FridgeOOP/Fridge.cs
@@ -6,7 +6,124 @@ using System.Threading.Tasks;
 
 namespace FridgeOOP
 {
-    class Fridge
+    public class Fridge
     {
-    }
+        //fields(Fields of baby bears)
+        private bool isClean;
+        private string dispenserType;
+        private int foodAmount;
+        private bool bulbsWork;
+        
+        //Properties
+        public bool IsClean
+        {
+            get { return this.isClean; }
+            set { this.isClean = value; }
+        }
+
+        public string DispenserType
+        {
+            get { return this.dispenserType; }
+        }
+
+        public int FoodAmount
+        {
+            get { return this.foodAmount; }
+            set { this.foodAmount = value; }
+        }
+
+        public bool BulbsWork
+        {
+            get { return this.bulbsWork; }
+            set { this.bulbsWork = value;}
+        }
+                
+        //Constructors
+        public Fridge()
+        {
+            this.dispenserType = "basic water dispenser";
+        }
+
+        public Fridge(string dispenserType)
+        {
+            this.dispenserType = dispenserType;
+        }
+
+        public Fridge(bool isClean, string dispenserType, int foodAmount, bool bulbsWork)
+        {
+            this.isClean = isClean;
+            this.dispenserType = dispenserType;
+            this.foodAmount = foodAmount;
+            this.bulbsWork = bulbsWork;
+        }
+        
+        //Methods()
+        // I need a way to dispense water
+	
+        public void DispenseWater()
+        {
+            if (dispenserType == "basic water dispenser")
+            {
+                Console.WriteLine("Here's some lukewarm water!");
+            }
+            else if (dispenserType == "deluxe dispenser")
+            {
+                Console.WriteLine("Enjoy your perfect ice water");
+            }
+            else
+            {
+                Console.WriteLine("What's gonna come out? It's a suprise!");
+            }
+        }
+
+        public string ChangeBulbs()
+        {
+            if (bulbsWork == false)
+            {
+                bulbsWork = true;
+                return "The bulbs have been changed";
+            }
+            else
+            {
+                return "The bulbs don't have to be changed.";
+            }
+        }
+
+        public void RemoveFood(int foodRemoved)
+        {
+            foodAmount -= foodRemoved;
+        }
+
+        public string CheckSupplies()
+        {
+            if (foodAmount >= 80)
+            {
+                return "fully stocked";
+            }
+            else if (foodAmount > 40)
+            {
+                return "getting low";
+            }
+            else
+            {
+                return "time to grocery shop";
+            }
+        }
+
+        public string Clean()
+        {
+            if (isClean == false)
+            {
+                isClean = true;
+                return "I cleaned the fridge!";
+            }
+            else
+            {
+                return "The fridge is already clean.";
+            }
+        }
+
+
+	}
+          
 }
