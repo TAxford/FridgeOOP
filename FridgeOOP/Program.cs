e35877e49563281f8a4f7917331db7a2cd0fd727
TAxford <t.axford2@yahoo.com>
2018-02-01 09:57:52 -0500

    Created reserve menu

--- FridgeOOP/Program.cs
+++ FridgeOOP/Program.cs
@@ -10,7 +10,17 @@ namespace FridgeOOP
     {
         static void Main(string[] args)
         {
-                      
+            //Create an interactive console window
+            //Promt user to chose what to do with the fridge
+            //Call methods on Fridge object to do what the user wants
+
+            Console.WriteLine("Hello, welcome to the fridge app.");
+            Console.WriteLine("What would you like to do with your fridge?");
+
+            
+
+
+
         }
     }
 }
